4ab6378386f092e308c81b1e4595718c0105637e
Surya Oktafendri <f2face@f2face.com>
2023-11-05 11:50:25 +0700

    Fix content

--- src/elements/BaseElement.ts
+++ src/elements/BaseElement.ts
@@ -18,8 +18,10 @@ export abstract class BaseElement implements SvgElement {
     private normalizeContent(content?: ElementContent) {
         if (typeof content === 'string') {
             return content;
+        } else if (typeof content === 'number') {
+            return String(content);
         } else if (content instanceof BaseElement) {
-            return content?.render();
+            return content.render();
         } else if (Array.isArray(content)) {
             const elements = content.map((el) => {
                 if (el instanceof BaseElement) return el.render();
